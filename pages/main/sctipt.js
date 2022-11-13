5a2e8e4e761aa70ac31c5510a727f4e99c1ed29b
msvanidze <mamiasvanidze@gmail.com>
2022-11-13 11:28:54 +0400

    Docs: Creation

---
+++ pages/main/sctipt.js
@@ -0,0 +1,6 @@
+// change the background color to red
+document.body.style.background = "red";
+
+// change it back after 1 second
+setTimeout(() => document.body.style.background = "", 1000);
+// setInterval(() => elem.hidden = !elem.hidden, 1000);
